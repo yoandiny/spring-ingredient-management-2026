a1b03a94570657fff879ece3465f2e301366f56d
Ryan Andriamahery <ryan@hei.school>
2026-04-01 08:35:52 +0300

    initial commit

---
+++ src/main/java/edu/hei/school/ingredients/IngredientsSpring2026Application.java
@@ -0,0 +1,13 @@
+package edu.hei.school.ingredients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientsSpring2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngredientsSpring2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hei/school/ingredients/IngredientsSpring2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hei.school.ingredients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientsSpring2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
